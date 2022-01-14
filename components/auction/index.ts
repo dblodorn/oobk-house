23062a167afa8812270aa8abf98b7da2608af1a4
Dain Blodorn Kim <dain@db13.us>
2022-01-14 11:31:53 -0800

    removed unused deps

--- components/auction/index.ts
+++ components/auction/index.ts
@@ -1,13 +1,9 @@
 import { ListButton } from './ListButton'
 import { BidButton } from './BidButton'
-import { ManageAuctionButton } from './ManageAuctionButton'
 import { TokenPreview } from './TokenPreview'
-import { OwnedTokenList } from './OwnedTokenList'
 
 export {
   BidButton,
   ListButton,
-  ManageAuctionButton,
   TokenPreview,
-  OwnedTokenList,
 }
